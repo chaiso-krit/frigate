ebdf33aa9c107ae1cd6fe70ffc8bf3e223dc0cd7
kaoitp <itpanw@gmail.com>
2025-03-10 14:23:00 +0700

    Add reset confirmation windows

--- web/src/utils/mqttUtil.ts
+++ web/src/utils/mqttUtil.ts
@@ -1,11 +1,20 @@
 import axios from 'axios';
 
-export const publishData = async ( path: string, data: string) => {
-  try {
-    const response = await axios.post(path, data);
-    return response.data;
-  } catch (error) {
-    console.error('Error publishing data:', error);
-    throw error;
+export const resetCount = async ( camera_name: String) => {
+  const confirmed = window.confirm(`Are you sure you want to reset the camera ${camera_name}?`);
+  if (confirmed) {
+    try {
+      const response = await axios.get(`/${camera_name}/reset`);
+      alert(`${camera_name} has been reset`); // แก้ไขคำว่า "reseted" เป็น "reset"
+      return response.data;
+    } catch (error) {
+      console.error('Error publishing data:', error);
+      alert(`Error resetting ${camera_name} failed.`);
+      throw error;
+    }
+  }
+  else {
+    alert('Camera reset canceled.');
+    return null;
   }
 };
